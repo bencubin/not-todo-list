5b1542317f1a9939b5d682a09e16840067d91d0f
Ben Cubin <bencubin@gmail.com>
2022-09-10 09:17:25 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log('Hellow world!');
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function AppHeader() {
+
+  return (
+    <h1>My Todo list</h1>
+  );
+};
+
+function SearchPanel() {
+
+  return (
+    <input placeholder='search' />
+  );
+}
+
+function TodoLIst() {
+
+  const items = ['Don\'t eat', 'Don\'t sleep'];
+
+  return (
+    <ul>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+    </ul>
+  );
+};
+
+function App() {
+
+  return (
+    <>
+      <AppHeader />
+      <SearchPanel />
+      <TodoLIst />
+    </>
+  );
+};
+
+root.render(<App />);
